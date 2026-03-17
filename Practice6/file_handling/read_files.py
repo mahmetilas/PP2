1aae48b51e8a4223da009d2ed61f18971c5fc168
Iliyas Makhmet <mahmetilas@gmail.com>
2026-03-17 11:29:35 +0500

    Add Practice6 - file handling and built-in functions examples

--- Practice6/file_handling/read_files.py
+++ Practice6/file_handling/read_files.py
@@ -3,4 +3,4 @@ with open('PP2/Practice6/file_handling/textfile.txt', 'w') as file:
     file.write("Woops! I have deleted the content!")
 with open('PP2/Practice6/file_handling/textfile.txt', 'r') as file:    
     print(file.readline())
-    file.close()
+    # file.close()
