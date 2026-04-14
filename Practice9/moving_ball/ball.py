d7a4d68f6ab791a71004f55d29aea220a7d3a171
Iliyas Makhmet <mahmetilas@gmail.com>
2026-04-14 11:32:51 +0500

    Add Practice9 - Pygame games: Mickey's clock, music player, moving ball

--- Practice9/moving_ball/ball.py
+++ Practice9/moving_ball/ball.py
@@ -5,32 +5,32 @@ screen = pygame.display.set_mode((1200, 700))
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-done = False
+done = True
 
 clock = pygame.time.Clock()
 
 circle_start_w = 600
 circle_start_h = 350
 
-while not done:
+while done:
     
     keys = pygame.key.get_pressed()
     pygame.draw.circle(screen, RED, (circle_start_w, circle_start_h), 25)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
+            done = False
     if keys[pygame.K_UP]:
-        if (circle_start_h > 38):
-            circle_start_h -= 20
+        if circle_start_h > 30:
+            circle_start_h -= 10
     if keys[pygame.K_DOWN]:
-        if (circle_start_h < 662):
-            circle_start_h += 20
+        if circle_start_h < 662:
+            circle_start_h += 10
     if keys[pygame.K_LEFT]:
-        if (circle_start_w > 38):
-            circle_start_w -= 20
+        if circle_start_w > 30:
+            circle_start_w -= 10
     if keys[pygame.K_RIGHT]:
-        if (circle_start_w < 1162):
-            circle_start_w += 20
+        if circle_start_w < 1162:
+            circle_start_w += 10
     
     screen.fill(WHITE)
     pygame.draw.circle(screen, RED, (circle_start_w, circle_start_h), 25)
